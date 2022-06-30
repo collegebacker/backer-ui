8f26ce1d95f330a0e6d9177a3fe7c5aa8b453d73
Pavel Laptev <pawellaptew@gmail.com>
2022-06-30 16:28:42 +0200

    :checkered_flag: new build

---
+++ ui/types/components/GhostInput/index.d.ts
@@ -0,0 +1,25 @@
+import React from "react";
+export interface Props {
+    className?: string;
+    type?: "text" | "password" | "number" | "email" | "money";
+    required?: boolean;
+    name: string;
+    label?: string;
+    value?: string;
+    autoFocus?: boolean;
+    placeholder?: string;
+    isInvalid?: boolean;
+    errorMessage?: string;
+    helperText?: string;
+    id?: string;
+    tabIndex?: number;
+    currency?: string;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    onSubmit?: (event: React.FormEvent<HTMLInputElement>) => void;
+    onEnterKeyPress?: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+    onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
+    onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
+    onInvalid?: (event: React.FormEvent<HTMLInputElement>) => void;
+}
+declare const GhostInput: React.ForwardRefExoticComponent<Props & React.RefAttributes<any>>;
+export default GhostInput;
--- ui/types/components/index.d.ts
+++ ui/types/components/index.d.ts
@@ -1,6 +1,7 @@
 export { default as Button } from "./Button";
 export { default as Icon } from "./Icon";
 export { default as Input } from "./Input";
+export { default as GhostInput } from "./GhostInput";
 export { default as CodeInput } from "./CodeInput";
 export { default as Logo } from "./Logo";
 export { default as ChipsSlider } from "./ChipsSlider";
