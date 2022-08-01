f39376d75afabad419604dff8c0c647f3f7447a2
Pavel Laptev <pawellaptew@gmail.com>
2022-08-01 16:25:23 +0200

    :construction: initial

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -3,64 +3,16 @@ import styles from "./styles.module.scss";
 
 export interface Props {
   className?: string;
-  style?: React.CSSProperties;
-  size?: "small" | "medium-small" | "medium" | "large";
-  color?: string;
 }
 
-const Spinner: React.FC<Props> = (props) => {
-  const getSize = (size: any) => {
-    switch (size) {
-      case "small":
-        return 24;
-      case "medium-small":
-        return 32;
-      case "medium":
-        return 40;
-      case "large":
-        return 80;
-    }
-  };
-
-  return (
-    <div
-      className={`${styles.loaderWrap} ${props.className}`}
-      style={{
-        ...props.style,
-        width: getSize(props.size),
-        height: getSize(props.size),
-      }}
-    >
-      <svg
-        width={getSize(props.size)}
-        height={getSize(props.size)}
-        viewBox={`0 0 ${getSize(props.size)} ${getSize(props.size)}`}
-        fill="none"
-        className={`${styles.loader} ${styles[props.size]}`}
-        style={props.color ? { fill: props.color } : {}}
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle
-          className={styles.path}
-          cx={getSize(props.size) / 2}
-          cy={getSize(props.size) / 2}
-          r={getSize(props.size) / 2 - 3}
-        />
-        <circle
-          className={styles.backPath}
-          cx={getSize(props.size) / 2}
-          cy={getSize(props.size) / 2}
-          r={getSize(props.size) / 2 - 3}
-        />
-      </svg>
-    </div>
-  );
+const Toast: React.FC<Props> = (props) => {
+  return <div className={`${styles.loaderWrap} ${props.className}`}></div>;
 };
 
-Spinner.defaultProps = {
+Toast.defaultProps = {
   className: "",
   style: {},
   size: "small",
 } as Partial<Props>;
 
-export default Spinner;
+export default Toast;
