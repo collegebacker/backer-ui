137275468950184b397c522e24c24aa491eafaf6
Pavel Laptev <pawellaptew@gmail.com>
2022-10-19 01:07:22 +0200

    Update stringToGradient.ts

--- src/utils/stringToGradient.ts
+++ src/utils/stringToGradient.ts
@@ -1,12 +1,60 @@
-const stringToGradient = (string: string, saturation = 90, lightness = 70) => {
-  let hash = 0;
+const colors = [
+  "#D09FCC",
+  "#B988D8",
+  "#9C6CCD",
+  "#F39271",
+  "#88D6F8",
+  "#77A3FA",
+  "#5D6AD8",
+  "#A7A0EF",
+  "#FBB199",
+  "#F18497",
+  "#E36C6C",
+  "#6171FD",
+  "#FFBD2E",
+  "#60B093",
+  "#85D1BF",
+];
 
-  for (let i = 0; i < string.length; i++) {
-    hash = string.charCodeAt(i) + ((hash << 1) - hash);
-    hash = hash & hash;
+const lightenDarkenHex = (hex: string, amount: number) => {
+  let usePound = false;
+
+  if (hex[0] === "#") {
+    hex = hex.slice(1);
+    usePound = true;
   }
-  const startColor = `hsl(${hash % 60}, ${saturation}%, ${lightness}%)`;
-  const endColor = `hsl(${hash % 260}, ${saturation}%, ${lightness}%)`;
+
+  const num = parseInt(hex, 16);
+
+  let r = (num >> 16) + amount;
+
+  if (r > 255) r = 255;
+  else if (r < 0) r = 0;
+
+  let b = ((num >> 8) & 0x00ff) + amount;
+
+  if (b > 255) b = 255;
+  else if (b < 0) b = 0;
+
+  let g = (num & 0x0000ff) + amount;
+
+  if (g > 255) g = 255;
+  else if (g < 0) g = 0;
+
+  return (usePound ? "#" : "") + (g | (b << 8) | (r << 16)).toString(16);
+};
+
+const stringToGradient = (string: string) => {
+  const trimmedString = string.trim().replace(/\s/g, "");
+
+  const startHash = trimmedString
+    .split("")
+    .reduce((acc, char) => acc + char.charCodeAt(0), 0);
+
+  const startColor = colors[startHash % colors.length];
+  const endColor = lightenDarkenHex(startColor, 50);
+
+  console.log(startColor, endColor);
 
   return `linear-gradient(200deg, ${startColor} 0%, ${endColor} 100%)`;
 };
