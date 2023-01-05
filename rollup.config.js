4c20e32c590b9ecf974388167790c5915c1dbe46
Pavel Laptev <pawellaptew@gmail.com>
2023-01-05 02:32:13 +0100

    🗜️ esbuild try

--- rollup.config.js
+++ rollup.config.js
@@ -1,57 +1,119 @@
-import resolve from '@rollup/plugin-node-resolve'
-import peerDepsExternal from 'rollup-plugin-peer-deps-external'
-import commonjs from '@rollup/plugin-commonjs'
-import typescript from '@rollup/plugin-typescript'
+// import resolve from '@rollup/plugin-node-resolve'
+// import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+// import commonjs from '@rollup/plugin-commonjs'
+// import typescript from '@rollup/plugin-typescript'
 import json from '@rollup/plugin-json'
-import dts from 'rollup-plugin-dts'
+// import dts from 'rollup-plugin-dts'
 import postcss from 'rollup-plugin-postcss'
 import autoprefixer from 'autoprefixer'
+// import { terser } from 'rollup-plugin-terser'
+// import filesize from 'rollup-plugin-filesize'
+
+// export default [
+//   {
+//     input: 'src/components/index.ts',
+//     external: ['react', 'react-dom', '@collegebacker/shared'],
+//     output: [
+//       {
+//         dir: 'ui',
+//         format: 'cjs',
+//         sourcemap: true
+//       }
+//     ],
+//     plugins: [
+//       peerDepsExternal({
+//         includeDependencies: false
+//       }),
+//       resolve(),
+//       postcss({
+//         plugins: [autoprefixer()],
+//         extract: 'ui.css',
+//         modules: true,
+//         minimize: true,
+//         use: ['sass']
+//       }),
+//       json(),
+//       commonjs(),
+//       typescript({
+//         compilerOptions: {
+//           declaration: false
+//         },
+//         exclude: [
+//           'node_modules/**',
+//           'ui',
+//           'src/**/*.stories.tsx',
+//           'src/**/*.test.tsx'
+//         ]
+//       }),
+//       terser({ compress: { drop_console: true } }),
+//       filesize()
+//     ]
+//   },
+//   {
+//     input: 'src/components/index.ts',
+//     output: [{ file: 'ui/index.d.ts', format: 'esm' }],
+//     plugins: [dts()]
+//   }
+// ]
+
+import dts from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
+
+import resolve from '@rollup/plugin-node-resolve'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
 import { terser } from 'rollup-plugin-terser'
 import filesize from 'rollup-plugin-filesize'
 
+const bundle = (config, folder) => ({
+  ...config,
+  input: `src/${folder}/index.ts`,
+  external: id => !/^[./]/.test(id)
+})
+
+const commonPlugins = [
+  peerDepsExternal(),
+  resolve(),
+  postcss({
+    plugins: [autoprefixer()],
+    extract: 'ui.css',
+    modules: true,
+    minimize: true,
+    use: ['sass']
+  }),
+  json(),
+  esbuild(),
+
+  terser({ compress: { drop_console: true } }),
+  filesize()
+]
+
 export default [
-  {
-    input: 'src/components/index.ts',
-    external: ['react', 'react-dom', '@collegebacker/shared'],
-    output: [
-      {
-        dir: 'ui',
-        format: 'cjs',
-        sourcemap: true
-      }
-    ],
-    plugins: [
-      peerDepsExternal({
-        includeDependencies: false
-      }),
-      resolve(),
-      postcss({
-        plugins: [autoprefixer()],
-        extract: 'ui.css',
-        modules: true,
-        minimize: true,
-        use: ['sass']
-      }),
-      json(),
-      commonjs(),
-      typescript({
-        compilerOptions: {
-          declaration: false
+  bundle(
+    {
+      plugins: commonPlugins,
+      output: [
+        {
+          file: `ui/index.js`,
+          format: 'cjs',
+          sourcemap: true
         },
-        exclude: [
-          'node_modules/**',
-          'ui',
-          'src/**/*.stories.tsx',
-          'src/**/*.test.tsx'
-        ]
-      }),
-      terser({ compress: { drop_console: true } }),
-      filesize()
-    ]
-  },
-  {
-    input: 'src/components/index.ts',
-    output: [{ file: 'ui/index.d.ts', format: 'esm' }],
-    plugins: [dts()]
-  }
+        {
+          file: `ui/index.mjs`,
+          format: 'es',
+          sourcemap: true
+        }
+      ]
+    },
+    'components'
+  ),
+  bundle(
+    {
+      plugins: [dts()],
+      output: {
+        file: `ui/index.d.ts`,
+        format: 'es'
+      }
+    },
+    'components'
+  )
 ]
