e15b2c54449879fdb2195c877e963eb3de773250
Pavel Laptev <pawellaptew@gmail.com>
2023-02-01 22:38:33 +0100

    :construction: button changes
    - removed `accent` button mode
    - `minWidth` renamed to `isMinWIdth`
    - added mobile `minWidth` prop as a number

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,87 +1,43 @@
-import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { ComponentMeta, ComponentStory } from '@storybook/react'
 
-import { Button } from "..";
-import React from "react";
-import icons from "../Icon/icons.json";
+import { Button } from '..'
+import React from 'react'
+import icons from '../Icon/icons.json'
 
-localStorage.clear();
+localStorage.clear()
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Forms/Button",
+  title: 'Forms/Button',
   component: Button,
   argTypes: {
     type: {
-      options: ["button", "submit", "reset"],
-      control: { type: "select" },
+      options: ['button', 'submit', 'reset'],
+      control: { type: 'select' }
     },
     target: {
-      options: ["_self", "_blank", "_parent", "_top"],
-      control: { type: "select" },
+      options: ['_self', '_blank', '_parent', '_top'],
+      control: { type: 'select' }
     },
     icon: {
       options: Object.keys(icons),
-      control: { type: "select" },
-    },
-  },
+      control: { type: 'select' }
+    }
+  }
   // More on argss: https://storybook.js.org/docs/react/api/argss
-} as ComponentMeta<typeof Button>;
+} as ComponentMeta<typeof Button>
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />
 
-export const Primary = Template.bind({});
+export const Default = Template.bind({})
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  mode: "primary",
-  label: "Primary",
-  size: "default",
-  tag: "a",
+Default.args = {
+  mode: 'primary',
+  label: 'Primary',
+  size: 'default',
+  tag: 'a',
   disabled: false,
   busy: false,
-  busyLabel: "",
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  mode: "accent",
-  label: "Accent",
-  size: "default",
-  tag: "a",
-  disabled: false,
-  busy: false,
-  busyLabel: "",
-};
-
-export const Outline = Template.bind({});
-Outline.args = {
-  mode: "outline",
-  label: "Outline",
-  size: "default",
-  tag: "a",
-  disabled: false,
-  busy: false,
-  busyLabel: "",
-};
-
-export const OutlineInverse = Template.bind({});
-OutlineInverse.args = {
-  mode: "outline-inverse",
-  label: "Outline inverse",
-  size: "default",
-  tag: "a",
-  disabled: false,
-  busy: false,
-  busyLabel: "",
-};
-
-export const Ghost = Template.bind({});
-Ghost.args = {
-  mode: "ghost",
-  label: "Ghost",
-  size: "default",
-  tag: "a",
-  disabled: false,
-  busy: false,
-  busyLabel: "",
-};
+  busyLabel: ''
+}
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,29 +1,30 @@
-import React from "react";
-import Icon from "../Icon";
+import React from 'react'
+import Icon from '../Icon'
 
-import styles from "./styles.module.scss";
-import Spinner from "../Spinner";
+import styles from './styles.module.scss'
+import Spinner from '../Spinner'
 
 export interface Props {
-  className?: string;
-  style?: React.CSSProperties;
-  mode?: "primary" | "accent" | "outline" | "ghost" | "outline-inverse";
-  size?: "default" | "small";
-  type?: "button" | "submit" | "reset";
-  target?: TargetTypes;
-  icon?: IconTypes;
-  href?: string;
-  disabled?: boolean;
-  label?: string;
-  maxWidth?: number;
-  minWidth?: boolean;
-  form?: string;
-  busy?: boolean;
-  busyLabel?: string;
-  tag?: "button" | "a";
-  onClick?: (event: any) => void;
-  onSubmit?: (event: any) => void;
-  onKeyPress?: (event: any) => void;
+  className?: string
+  style?: React.CSSProperties
+  mode?: 'primary' | 'outline' | 'ghost' | 'outline-inverse'
+  size?: 'default' | 'small'
+  type?: 'button' | 'submit' | 'reset'
+  target?: TargetTypes
+  icon?: IconTypes
+  href?: string
+  disabled?: boolean
+  label?: string
+  maxWidth?: number
+  minWidth?: number
+  isMinWidth?: boolean
+  form?: string
+  busy?: boolean
+  busyLabel?: string
+  tag?: 'button' | 'a'
+  onClick?: (event: any) => void
+  onSubmit?: (event: any) => void
+  onKeyPress?: (event: any) => void
 }
 
 const ButtonContent = ({ ...props }: Props) => {
@@ -41,7 +42,7 @@ const ButtonContent = ({ ...props }: Props) => {
           style={
             props.label
               ? {
-                  marginLeft: "10px",
+                  marginLeft: '10px'
                 }
               : {}
           }
@@ -51,49 +52,50 @@ const ButtonContent = ({ ...props }: Props) => {
       {props.busy ? (
         <Spinner
           className={styles.spinner}
-          size="medium-small"
+          size='medium-small'
           style={
             props.busyLabel && props.label
               ? {
-                  marginLeft: "10px",
+                  marginLeft: '10px'
                 }
               : {}
           }
         />
       ) : null}
     </>
-  );
-};
+  )
+}
 
 const Button = React.forwardRef<any, Props>((props, ref) => {
   const classes = `${props.className} ${styles.button} ${styles[props.mode]} ${
     styles[props.size]
-  } ${props.disabled ? styles.disabled : ""} ${
+  } ${props.disabled ? styles.disabled : ''} ${
     props.label
-      ? props.minWidth
+      ? props.isMinWidth
         ? styles[`minWidth-${props.size}`]
-        : ""
+        : ''
       : styles[`fixedSize-${props.size}`]
-  }`;
+  }`
 
   const style = {
     ...(props.maxWidth ? { maxWidth: props.maxWidth } : {}),
-    ...(props.busy ? { pointerEvents: "none" } : {}),
-    ...(props.style || {}),
-  } as React.CSSProperties;
+    ...(props.minWidth ? { minWidth: props.minWidth } : {}),
+    ...(props.busy ? { pointerEvents: 'none' } : {}),
+    ...(props.style || {})
+  } as React.CSSProperties
 
   const conditionalProps =
-    props.tag === "a"
+    props.tag === 'a'
       ? {
-          rel: "noreferrer",
+          rel: 'noreferrer',
           href: props.href,
           target: props.target,
-          onSubmit: props.onSubmit,
+          onSubmit: props.onSubmit
         }
       : {
           type: props.type,
-          form: props.form,
-        };
+          form: props.form
+        }
 
   return (
     <props.tag
@@ -107,22 +109,22 @@ const Button = React.forwardRef<any, Props>((props, ref) => {
     >
       <ButtonContent {...props} />
     </props.tag>
-  );
-});
+  )
+})
 
 Button.defaultProps = {
-  className: "",
-  mode: "primary",
-  size: "default",
-  tag: "button",
+  className: '',
+  mode: 'primary',
+  size: 'default',
+  tag: 'button',
   busy: false,
-  type: "button",
-  href: "#",
-  target: "_self",
-  busyLabel: "",
+  type: 'button',
+  href: '#',
+  target: '_self',
+  busyLabel: '',
   disabled: false,
-  label: "Button",
-  minWidth: true,
-} as Partial<Props>;
+  label: 'Button',
+  isMinWidth: true
+} as Partial<Props>
 
-export default Button;
+export default Button
