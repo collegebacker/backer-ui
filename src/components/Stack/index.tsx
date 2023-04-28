50817a93a1a0e5a4f93c6d07fe252cec0ba8c8b8
Pavel Laptev <pawellaptew@gmail.com>
2023-04-28 12:41:48 +0200

    :checkered_flag: added `0` to `Stack`

--- src/components/Stack/index.tsx
+++ src/components/Stack/index.tsx
@@ -18,7 +18,7 @@ export interface Props extends React.HTMLAttributes<HTMLElement> {
     | 'label'
     | 'fieldset'
   direction?: 'row' | 'column'
-  spacing?: 5 | 10 | 15 | 20 | 25 | 30 | 40 | 50 | 60 | 70 | 80 | 90 | 100
+  spacing?: 0 | 5 | 10 | 15 | 20 | 25 | 30 | 40 | 50 | 60 | 70 | 80 | 90 | 100
   children: React.ReactNode
 }
 
