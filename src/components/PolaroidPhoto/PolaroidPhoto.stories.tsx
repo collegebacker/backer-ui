7463cecd25491af717ff20ce6edecdf3f0dc4f34
Pavel Laptev <pawellaptew@gmail.com>
2023-05-11 02:32:26 +0200

    :checkered_flag: updated `getFirstLetter` function

--- src/components/PolaroidPhoto/PolaroidPhoto.stories.tsx
+++ src/components/PolaroidPhoto/PolaroidPhoto.stories.tsx
@@ -1,34 +1,34 @@
-import React from "react";
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from 'react'
+import { ComponentStory, ComponentMeta } from '@storybook/react'
 
-import { PolaroidPhoto, ImageEditor } from "..";
+import { PolaroidPhoto, ImageEditor } from '..'
 
-localStorage.clear();
+localStorage.clear()
 
 export default {
-  title: "Misc/PolaroidPhoto",
-  component: PolaroidPhoto,
-} as ComponentMeta<typeof PolaroidPhoto>;
+  title: 'Misc/PolaroidPhoto',
+  component: PolaroidPhoto
+} as ComponentMeta<typeof PolaroidPhoto>
 
 const Template: ComponentStory<typeof PolaroidPhoto> = (args) => {
   const [currentImage, setCurrentImage] = React.useState<string | undefined>(
     null
-  );
-  const [newImage, setNewImage] = React.useState<string | undefined>(null);
-  const [isEditorOpen, setIsEditorOpen] = React.useState<boolean>(false);
+  )
+  const [newImage, setNewImage] = React.useState<string | undefined>(null)
+  const [isEditorOpen, setIsEditorOpen] = React.useState<boolean>(false)
 
   const handleImageSelect = (imageFile: File) => {
-    setIsEditorOpen(true);
-    setNewImage(URL.createObjectURL(imageFile));
-  };
+    setIsEditorOpen(true)
+    setNewImage(URL.createObjectURL(imageFile))
+  }
 
   const handleSubmit = () => {
     // imulate image upload
     setTimeout(() => {
-      setCurrentImage(newImage);
-      setIsEditorOpen(false);
-    }, 2000);
-  };
+      setCurrentImage(newImage)
+      setIsEditorOpen(false)
+    }, 2000)
+  }
 
   return (
     <>
@@ -44,16 +44,16 @@ const Template: ComponentStory<typeof PolaroidPhoto> = (args) => {
         onChange={handleImageSelect}
       />
     </>
-  );
-};
-export const Default = Template.bind({});
+  )
+}
+export const Default = Template.bind({})
 Default.args = {
   uploadMode: false,
-  className: "",
+  className: '',
   style: {
     // @ts-ignore: Unreachable code error
-    "--size": "200px",
-    margin: "40px",
+    '--size': '200px',
+    margin: '40px'
   },
-  name: "Lily Sharpe’sEducation Fund 🐰🌿",
-};
+  name: 'Lily Sharpe’sEducation Fund 🐰🌿'
+}
--- src/utils/getFirstLetter.ts
+++ src/utils/getFirstLetter.ts
@@ -2,13 +2,17 @@ import getRandomEmoji from './getRandomEmoji'
 
 const getFirstLetter = (name: string) => {
   // remove spaces before and after the name
+  if (!name) {
+    return getRandomEmoji()
+  }
+
   const trimmedName = name.trim()
 
   if (trimmedName.length === 0) {
     return getRandomEmoji()
-  } else {
-    return Array.from(trimmedName)[0].toUpperCase()
   }
+
+  return Array.from(trimmedName)[0].toUpperCase()
 }
 
 export default getFirstLetter
